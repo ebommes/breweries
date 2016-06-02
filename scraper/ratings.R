36f84106676357a8a13bee4163a764db44e4518c
Elisabeth Bommes <elisabeth.bommes@hu-berlin.de>
2016-06-02 09:24:33 +0200

    save results in sql database

--- scraper/ratings.R
+++ scraper/ratings.R
@@ -10,6 +10,7 @@ setwd(paste(path.project, "csv", sep = ""))
 
 # functions
 grab.rating <- function(html){
+
     nodes <- getNodeSet(html, "//div[@id='rating_fullview_content_2']")
     nodes <- lapply(nodes, function(x) xmlParse(saveXML(x)))
     nodes <- lapply(nodes, function(x) xpathApply(x, path = "//span[@class='muted']", xmlValue))
@@ -17,7 +18,7 @@ grab.rating <- function(html){
     rating <- strsplit(sapply(nodes, "[[", 1), "|", fixed = TRUE)
     # rating <- as.data.frame(t(sapply(rating, function(x) gsub("[^0-9.]", "", x))))
     rating <- sapply(rating, function(x) gsub("[^0-9.]", "", x))
-    
+
     if(class(rating) != "matrix"){
         rating <- do.call(rbind, rating)
     }else{
@@ -36,6 +37,7 @@ grab.rating <- function(html){
 }
 
 grab.text <- function(html){
+
     nodes <- getNodeSet(html, "//div[@id='rating_fullview_content_2']")
     nodes <- lapply(nodes, function(x) xmlParse(saveXML(x)))
     xpath <- "//div[@id='rating_fullview_content_2']/text()"
@@ -53,6 +55,7 @@ grab.text <- function(html){
 }
 
 grab.style <- function(html){
+
     links <- xpathApply(html, "//a", xmlGetAttr, "href")
     links <- unique(links[grep("/beer/style", links)])
     style <- as.numeric(unlist(gsub("[^0-9]", "", links)))
@@ -61,6 +64,7 @@ grab.style <- function(html){
 }
 
 maxpage <- function(html){
+
     links <- xpathApply(html, path = "//a", xmlGetAttr, "href")
     links <- unlist(links[grepl("&start=", links, fixed = TRUE) &
                           grepl("beer&sort", links, fixed = TRUE)])
@@ -73,49 +77,63 @@ maxpage <- function(html){
     return(maxp)
 }
 
-beers.df <- read.csv("beers.csv")
+grab.review <- function(html, beers.df, sql.l,  i, j){
+    url <- "http://www.beeradvocate.com/beer/profile/28743/136936/"
 
-i = 2
+    # maxp <- 1
+    iter <- 0
+    html <- try(grab(url), silent = TRUE)
 
-#url <- sprintf("http://www.beeradvocate.com/beer/profile/%s/%s/", beers.df$brewery[i], beers.df$beer[i])  
+    j = 0
 
-url <- "http://www.beeradvocate.com/beer/profile/28743/136936/"
-
-maxp <- 1
-iter <- 0
-html <- try(grab(url), silent = TRUE)
-
-j = 0
-while(j <= maxp){
     if(j == 0) {
         maxp <- try(maxpage(html))
         # check if there are reviews
         checkreview = xpathApply(html, "//h6", xmlValue)
     }
-    
-    if(j != 0){
-        url <- paste(url, "?view=beer&sort=&start=", j, sep = "")
-        html <- try(grab(url), silent = TRUE)
+
+    while(j <= maxp){
+        con <- dbConnect(MySQL(), host = sql.l$host, user = sql.l$user, 
+                         password = sql.l$pwd, dbname = sql.l$db)
+
+        if(j != 0){
+            url <- paste(url, "?view=beer&sort=&start=", j, sep = "")
+            html <- try(grab(url), silent = TRUE)
+        }
+
+        if(length(grep("No Reviews", checkreview)) == 0){
+            # rating
+            rating <- grab.rating(html)
+
+            # text
+            txt <- grab.text(html)
+
+            # style: better would be extra table for beer info.
+            style <- grab.style(html)
+        }
+
+        res <- data.frame(review  = (j+1):(j+length(txt)), 
+                          brewery = rep(beers.df$brewery[i], length(txt)),
+                          beer    = rep(beers.df$beer[i], length(txt)),
+                          rating, 
+                          txt,
+                          style = rep(style, length(txt)))
+
+        dbWriteTable(con, value = res, name = "reviews", row.names = FALSE, append = TRUE) 
+        dbDisconnect(con)
+
+        j <- j + 25
+        
+        Sys.sleep(1)
     }
 
-    if(length(grep("No Reviews", checkreview)) == 0){
-        # rating
-        rating <- grab.rating(html)
-
-        # text
-        txt <- grab.text(html)
-
-        # style
-        style <- grab.style(html)
-    }
-
-    j <- j + 25
-    
-    Sys.sleep(1)
-    print(j)
 }
 
+beers.df <- read.csv("beers.csv")
 
-
-
-
+n <- length(beers.df$beer)
+  
+for(i in 1:n){
+    print(i)
+    grab.review(html, beers.df, sql.l,  i, j)
+}
