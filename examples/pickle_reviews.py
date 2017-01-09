498dc62f1b05cbb94ce90cf7bdf7be9e9cd00eea
Elisabeth Bommes <elisabeth.bommes@gmail.com>
2017-01-09 11:27:20 +0100

    switch from custom beer class to textacy corpus

--- examples/pickle_reviews.py
+++ examples/pickle_reviews.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Pickle reviews of 50 beers for nlp in SpaCy."""
+"""Pickle reviews of 100 beers for nlp in SpaCy."""
 
 # set path to project -> change if needed
 project_path = '/Users/EB/Google Drive/Projects/breweries'
@@ -10,16 +10,26 @@ from pymongo import MongoClient
 
 # pickle
 import pickle
+import os
 
 # random sampling
 import random
 
-# change directory
-import os
-os.chdir(project_path + '/modules/')
+# create corpus
+import textacy
 
-# beer
-from beeradvocate.classes import Beer
+# functions
+def mdata(item, review):
+    """Returns meta data in dictionary format."""
+    meta = {'brew_id': item['brew_id'], 
+            'beer_id': item['beer_id'], 
+            'style_id': item['style_id'], 
+            'brew_name': item['brew_name'], 
+            'beer_name': item['beer_name'], 
+            'style_name': item['style_name'],
+            'rating': review[1] 
+           }
+    return meta
 
 # load data
 client = MongoClient()
@@ -29,23 +39,22 @@ cursor = db.reviews.find()
 # only keep beers with reviews
 data = [item for item in cursor if 'reviews' in item]
 
-# random sample of 50 beers
-data = random.sample(data, 50)
+# random sample of 100 beers
+data = random.sample(data, 100)
 
-# nicer data format
-beers = []
+# textacy corpus format
+meta = []
+texts = []
 
 for item in data:
     reviews = None
     reviews = [(i['text'], i['rating']) for i in item['reviews'][0]]
-    beers.append(Beer(brew_id = item['brew_id'], 
-                      beer_id = item['beer_id'], 
-                      style_id = item['style_id'], 
-                      brew_name = item['brew_name'], 
-                      beer_name = item['beer_name'], 
-                      style_name = item['style_name'], 
-                      reviews = reviews))
+    for review in reviews:
+        meta.append(mdata(item, review))
+        texts.append(review[0])
 
-# pickle results to data/reviews_sample.p
+# saving textacy corpus currently only works with ascii text -> pickle instead
 os.chdir(project_path + '/data/')
-pickle.dump(beers, open('reviews_sample.p', 'wb'))
+
+with open('reviews_sample.p', 'wb') as f:
+    pickle.dump([meta, texts], f)
