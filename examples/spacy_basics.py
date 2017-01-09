156ea5c37730c80e44be83a944287c0b4b57a146
Elisabeth Bommes <elisabeth.bommes@gmail.com>
2017-01-09 11:54:30 +0100

    remove custom class

--- examples/spacy_basics.py
+++ examples/spacy_basics.py
@@ -10,11 +10,6 @@ import pickle
 import spacy
 import os
 
-# change directory and load Beer class
-os.chdir(project_path + '/modules/')
-
-from beeradvocate.classes import Beer
-
 # create "custom" spacy pipeline (would also be standard)
 def spacy_pipe(nlp):
     return(nlp.tagger, nlp.parser, nlp.entity)
@@ -24,31 +19,25 @@ nlp = spacy.load('en', create_pipeline = spacy_pipe)
 
 # load pickled beer reviews
 os.chdir(project_path + '/data/')
-beers = pickle.load(open('reviews_sample.p', 'rb'))
+data = pickle.load(open('reviews_sample.p', 'rb'))
 
 # start with one review to check functionality
-review = beers[0].reviews[0][0]
+review = data[1][10]
 review = nlp(review)
 
 # Lemmatize the review and keep only (proper) nouns and adjectives
 # This might be "enough" pre-processing for e.g. cluster analysis
-lemmas = []
-for word in review:
-    if word.pos_ in ('NOUN', 'PROPN', 'ADJ'):
-        lemmas.append(word.lemma_)
-
-print(lemmas)
+interesting_pos = ('NOUN', 'PROPN', 'ADJ')
+print([word.lemma_ for word in review if word.pos_ in interesting_pos])
 
 # Parser
 # Extract noun chunks in the text (with length > 1)
 # Note: if dependency parsing is not needed, use:
 #       spacy.load('en', parser = False) to increase speed
-for np in review.noun_chunks:
-    if len(np) > 1:
-        print(np.lemma_)
+print([np.lemma_ for np in review.noun_chunks if len(np) > 1])
 
-# some of these dependencies (e.g. "creamy head", "earthy spice") 
-# are more interesting than others (e.g. "this one")
+# some of these dependencies (e.g. "dark fruit") 
+# are more interesting than others (e.g. "the chance")
 # we can use a rule based system to extract them
 
 for np in review.noun_chunks:
@@ -57,3 +46,8 @@ for np in review.noun_chunks:
 
     if  tok_count == len(toks) & len(toks) > 1:
         print(np.lemma_)
+
+
+# Entity recognition
+# Currently not interesting, might be interesting for other projects
+print([(entity, entity.label_) for entity in review.ents])
